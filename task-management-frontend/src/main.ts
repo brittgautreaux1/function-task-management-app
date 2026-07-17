915348869a4101f8c095eddabffb3532aafcacc4
Britt Gautreaux <brittgautreaux@gmail.com>
2026-07-17 15:57:18 -0600

    retoring user if it's lost in pinia store

--- task-management-frontend/src/main.ts
+++ task-management-frontend/src/main.ts
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+import { useAuthStore } from './stores/authStore'
 
 const app = createApp(App)
 
@@ -13,3 +14,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+const authStore = useAuthStore();
+authStore.restoreUser();
